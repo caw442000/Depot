bb770476ac7826dbb131663baab8103fb18d5b9b
Cedric Winbush <cwvisionnfocus@gmail.com>
2013-09-05 16:04:35 -0400

    Start Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  def increment_count
+  if session[:counter].nil?
+    session[:counter] = 0
+  end
+  session[:counter] += 1
+end
+
   def index
+    @count = increment_count
     @products = Product.order(:title)
+    @count_message = "You've been here #{@count} times" if session[:counter] >5
   end
 end
